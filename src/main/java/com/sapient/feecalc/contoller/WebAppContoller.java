53f9bfc7c2ceea086dee2a2bad28494fcfe32cb8
Kiran Kumar Polusani <kirankumarpolusani@gmail.com>
2019-08-24 08:52:35 +0530

    OpenId Connect Integration

--- src/main/java/com/sapient/feecalc/contoller/WebAppContoller.java
+++ src/main/java/com/sapient/feecalc/contoller/WebAppContoller.java
@@ -1,11 +1,14 @@
 package com.sapient.feecalc.contoller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +18,19 @@ import java.util.Date;
 
 @Controller
 public class WebAppContoller {
+
     private String appMode;
-
-
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public WebAppContoller(Environment environment){
         appMode = environment.getProperty("app-mode");
     }
 
-    @RequestMapping("/")
+    @RequestMapping("/login")
     public String index(Model model){
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.info(username);
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Kiran Kumar");
         model.addAttribute("projectname", "Emotion Recognition");
